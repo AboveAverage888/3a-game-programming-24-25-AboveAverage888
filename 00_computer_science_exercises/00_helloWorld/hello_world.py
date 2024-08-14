c2cae769c276a8454dafad0cdd87036258d28923
AboveAverage888 <s11761509@students.duvalschools.org>
2024-08-14 12:56:23 -0400

    v0.2
    Using print()

--- 00_computer_science_exercises/00_helloWorld/hello_world.py
+++ 00_computer_science_exercises/00_helloWorld/hello_world.py
@@ -1,3 +1,10 @@
 # Hello World, Jackques Williams, v0.1
 
 print() # print() is a built-in FUNCTION for Python.
+
+# SYNTAX ERRORS -- error with the way code is written, such as as missing characters or incorrect case.
+print("Hello World")
+print("New Line Character\n") #\n adds an empty line after printing.
+
+print("How many \n lines will\n this print on\n the screen?\n")
+
