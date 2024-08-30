84e4fae16b1b65cc854156330f498e8c98a74d97
AboveAverage888 <s11761509@students.duvalschools.org>
2024-08-30 11:27:20 -0400

    Program Temperature

--- 00_computer_science_exercises/02_flow_control_structures/flowContorlStructures.py
+++ 00_computer_science_exercises/02_flow_control_structures/flowContorlStructures.py
@@ -1,7 +1,7 @@
 # Flow Control Structures, Jackques Williams, v0.0
 # Making Computer Programs Make Decisions
 
-temperature = 188.29
+temperature = 72
 color = "Black"
 height = 8
 likesPineappleOnPizza = True 
@@ -27,6 +27,34 @@ else:
      print("Your shirt is not the correct  unitform color\n")
 
 if temperature == 188.92:
-    print("It is hot as heck")
+    print("It is hot as heck\n")
 else:
-    print("It is too cold")
+    print("It is too cold\n")
+
+# AMUSEMENT PARK HEIGHT CHECKER EXAMPLE
+# Must be > min. height and < max. height to ride
+
+if height <= 6:
+    print("You're too tall enough to ride the Sling Shot!\n")
+elif height <= 3:
+    print("You're too tall to ride the Sling Shot\n")
+else: # "dang it dude, something wrong!"
+    print("Error,height not detected. DON'T RIDE.\n")
+
+# When writing if-elif-else block checks for the HIGHEST VALUE first when using > or >=
+# When writing if-elif-else block checks for the LOWEST VALUE first when using < or <=
+
+# Create an if-elif-else block that check for temperature
+# If the temperature is at least 100, print that its too hot outside.
+# If the temperature is at least 50 degress but less than 100, print that recess is allowed 
+# If the temperature is less than 50 degress but greater than 0, print that recess is in the gym    
+# If no temperature is detected, print an error message
+
+if temperature >= 100:
+    print("It is too hot outside\n")
+elif temperature >= 50:
+    print("Recess is allowed\n")
+elif temperature <= 50:
+    print("Recess is allowed in the gym\n")
+else:
+    print("Error temperature is not found\n")
