ef134c8e4c4f9076ea7f688b6233649a021c3018
AboveAverage888 <s11761509@students.duvalschools.org>
2024-09-04 11:55:06 -0400

    Input Practice

---
+++ 00_computer_science_exercises/01_dataTypes_Operators/user_input.py
@@ -0,0 +1,30 @@
+# User Input Practice, Jackques Williams,  v0.0
+
+# input() id the bulit-in function to get information from the KEYBOARD.
+# BASIC EXAMPLE
+variableName = input("Please type a variable name and press enter.\n") 
+print(variableName)
+
+# input() saves ALL INPUT TO STRING DATA TYPES BY DEFAULT!!!
+# input() saves ALL INPUT TO STRING DATA TYPES BY DEFAULT!!!
+# input() saves ALL INPUT TO STRING DATA TYPES BY DEFAULT!!!
+# input() saves ALL INPUT TO STRING DATA TYPES BY DEFAULT!!!
+
+# INCORRECT, CAUSES A CONCATENATION ERROR.
+#myNumber = input("Press type an INTERGER number, then press enter.\n")
+#print(myNumber + 5)
+
+# CORRECT -- Use a wrapper
+myNumber = int(input("Press type an INTERGER number, then press enter.\n"))
+print(myNumber + 5)
+
+# Wrapper Functions
+# int() will convert the data to an INTERGER if possible
+# float() will convert the data to a FLOAT if possible
+# str() will convert the data to a STRING if possible
+
+
+
+
+
+
